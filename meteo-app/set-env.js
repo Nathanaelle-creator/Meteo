6aa50c3c6af39378236359c015e1fa1fb51f2aa7
Nathanaelle-creator <nathanaelle.latouche@epitech.eu>
2025-08-28 12:29:42 +0200

    ADD:ajout du fichier pour plus securiser le code

---
+++ meteo-app/set-env.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const apiKey = process.env.OPEN_WEATHER_API_KEY || '';
+
+const envFileContent = `
+export const environment = {
+  production: true,
+  openWeatherApiKey: '${apiKey}'
+};
+`;
+
+fs.writeFileSync('./src/environments/environment.prod.ts', envFileContent);
+
+console.log('✅ environment.prod.ts file generated with the API key.');
--- meteo-app/src/app/meteo.service.ts
+++ meteo-app/src/app/meteo.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from './environments/environment.prod';
+
 
 @Injectable({
 providedIn: 'root'
 })
 export class MeteoService {
-private apiKey = '9332e6cf8bad81609c0bfbf09cdf4532';
+private apiKey = environment.openWeatherApiKey;
 private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 constructor(private http: HttpClient) {}
